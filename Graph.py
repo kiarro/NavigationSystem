361e2817bbd16abc5c55f04081b6960d2b75ceea
Юрий <38157715+kiarro@users.noreply.github.com>
2019-03-30 16:17:35 +0300

    Add files via upload
    Downloads that are needed:
    1. numpy
    2. Image

--- Graph.py
+++ Graph.py
@@ -1,112 +1,118 @@
-from PIL import Image
+def Dima_func(filename):
+    from PIL import Image
 
-# Dunno
-highestPoint = 11000
+    # Dunno
+    highestPoint = 25
 
-# TxT files
-name_of_txt_file_with_heights = "HeightMap.txt"
-name_of_txt_file_with_danger_of_heights = "DepthDangerZone.txt"
+    # Input txt files (they must have same height and width)
+    name_of_txt_file_with_heights = filename
+    name_of_txt_file_with_danger_of_heights = "DepthDangerZone.txt"
 
-# Images (Program won't work with formats, that don't support RGBA format)
-name_of_map_image = "Map.png"
-name_of_depth_danger_image = "DangerDepthMask.png"
-name_of_the_final_image = "Finale.png"
+    # Output images (Program won't work with formats, that don't support RGBA format)
+    name_of_map_image = "Map.png"
+    name_of_depth_danger_image = "DangerDepthMask.png"
+    name_of_the_final_image = "Finale.png"
 
-# Map colours
-ground_colour = 160, 82, 45, 255
+    # Map colours
+    ground_colour = 160, 82, 45, 255
 
-# Colours of zones
-safe_zone_colour = 255, 255, 255, 0  # Transparent white
-middle_danger_zone_colour = 255, 255, 0, 150  # Half-transparent yellow
-full_danger_zone_colour = 255, 0, 0, 150  # Half-transparent red
+    # Colours of zones
+    safe_zone_colour = 255, 255, 255, 0  # Transparent white
+    low__danger_zone_colour = 255, 255, 204, 150  # Half-transparent light yellow
+    middle_danger_zone_colour = 255, 255, 0, 150  # Half-transparent yellow
+    high_danger_zone_colour = 255, 140, 0, 150  # Half-transparent orange
+    full_danger_zone_colour = 255, 0, 0, 150  # Half-transparent red
 
-
-def map_colouring(depth):
-    if depth == 0:
-        return ground_colour
-    else:
-        temp = int(depth / highestPoint * 255)
-        r = 0
-        g = 255 - temp
-        b = 255 - temp
-        return r, g, b, 255
-
-
-def danger_colouring(danger_meter):
-    if danger_meter <= 0.25:  # Check if safe zone
-        return safe_zone_colour
-    elif danger_meter < 0.75:  # Check if middle-danger zone
-        return middle_danger_zone_colour
-    else:  # Danger zone!
-        return full_danger_zone_colour
+    zones = {
+            0: safe_zone_colour,
+            0.25: low__danger_zone_colour,
+            0.5: middle_danger_zone_colour,
+            0.75: high_danger_zone_colour,
+            1: full_danger_zone_colour
+            }
 
 
-def list_nums(txt_name):
-    read = open(txt_name, "r")
-    content = read.readlines()  # List of strings with depth numbers
-    read.close()
-    return content
+    def map_colouring(depth):
+        if depth == 0:
+            return ground_colour
+        else:
+            temp = int(depth / highestPoint * 255)
+            r = 0
+            g = 255 - temp
+            b = 255 - temp
+            return r, g, b, 255
 
 
-def draw_map(num_list, img_name):
-
-    im = Image.new('RGBA', (len(num_list[0].split()), len(num_list)))  # Create empty matrix-sized image
-    x = 0  # Python index system is... questionable,
-    y = 0  # so there is a workaround
-    for i in num_list:
-        for j in i.split():
-            im.putpixel((x, y), map_colouring(int(j)))  # Colouring corresponding pixels
-            x += 1
-        x = 0  # Go back at the end of each line
-        y += 1  # Next line
-    im.save(img_name)  # or any image format
-    im.close()
+    def danger_colouring(danger_meter):
+        return zones.get(danger_meter)  # Get a zone colour from dict
 
 
-def draw_danger(num_list, img_name):
-    im = Image.new('RGBA', (len(num_list[0].split()), len(num_list)))  # Create empty matrix-sized image
-    x = 0  # Python index system is... questionable,
-    y = 0  # so there is a workaround
-    for i in num_list:
-        for j in i.split():
-            im.putpixel((x, y), danger_colouring(float(j)))  # Colouring corresponding pixels
-            x += 1
-        x = 0  # Go back at the end of each line
-        y += 1  # Next line
-    im.save(img_name)  # or any image format
-    im.close()
+    def list_nums(txt_name):
+        read = open(txt_name, "r")
+        content = read.readlines()  # List of strings with depth numbers
+        read.close()
+        return content
 
 
-def combine(image1_n, image2_n):
-    image1 = Image.open(image1_n)  # Open both images
-    image2 = Image.open(image2_n)
-    width1, height1 = image1.size  # Get resolutions of images
-    width2, height2 = image2.size
-    if width1 == width2 and height1 == height2:  # Check if same resolution
-        finale = Image.new('RGBA', (width1, height1))  # Create image for filling
-        for y in range(height1):
-            for x in range(width1):
-                r1, g1, b1, a1 = image1.getpixel((x, y))  # Get pixel values
-                r2, g2, b2, a2 = image2.getpixel((x, y))
-                if a2 == 0:     # Check if safe zone and fill pixel
-                    finale.putpixel((x, y), (r1, g1, b1, a1))
-                else:
-                    finale.putpixel((x, y), (r2, g2, b2, a2))
-        finale.save(name_of_the_final_image)  # Save image and close all
-        finale.close()
-    image1.close()
-    image2.close()
+    def draw_map(num_list, img_name):
+        im = Image.new('RGBA', (len(num_list[0].split()), len(num_list)))  # Create empty matrix-sized image
+        x = 0  # Python index system is... quite cumbersome,
+        y = 0  # so there is a workaround
+        for i in num_list:
+            for j in i.split():
+                im.putpixel((x, y), map_colouring(int(j)))  # Colouring corresponding pixels
+                x += 1
+            x = 0  # Go back at the end of each line
+            y += 1  # Next line
+        im.save(img_name)  # or any image format
+        im.close()
 
 
-def main():
-    # Read file
-    matrix_map = list_nums(name_of_txt_file_with_heights)
-    # Draw matrix
-    draw_map(matrix_map, name_of_map_image)
-    danger_map = list_nums(name_of_txt_file_with_danger_of_heights)
-    draw_danger(danger_map, name_of_depth_danger_image)
-    combine(name_of_map_image, name_of_depth_danger_image)
+    def draw_danger(num_list, img_name):
+        im = Image.new('RGBA', (len(num_list[0].split()), len(num_list)))  # Create empty matrix-sized image
+        x = 0  # Python index system is... quite cumbersome,
+        y = 0  # so there is a workaround
+        for i in num_list:
+            for j in i.split():
+                im.putpixel((x, y), danger_colouring(float(j)))  # Colouring corresponding pixels
+                x += 1
+            x = 0  # Go back at the end of each line
+            y += 1  # Next line
+        im.save(img_name)  # or any image format
+        im.close()
 
 
-if __name__ == '__main__':
-    main()
+    def combine(image1_n, image2_n):
+        image1 = Image.open(image1_n)  # Open both images
+        image2 = Image.open(image2_n)
+        width1, height1 = image1.size  # Get resolutions of images
+        width2, height2 = image2.size
+        if width1 == width2 and height1 == height2:  # Check if same resolution
+            finale = Image.new('RGBA', (width1, height1))  # Create image for filling
+            for y in range(height1):
+                for x in range(width1):
+                    r1, g1, b1, a1 = image1.getpixel((x, y))  # Get pixel values
+                    r2, g2, b2, a2 = image2.getpixel((x, y))
+                    if a2 == 0:     # Check if safe zone and fill pixel
+                        finale.putpixel((x, y), (r1, g1, b1, a1))
+                    else:
+                        finale.putpixel((x, y), (r2, g2, b2, a2))
+            finale.save(name_of_the_final_image)  # Save image and close all
+            finale.close()
+        image1.close()
+        image2.close()
+
+
+    def full_map_conjunction():
+        # Read file
+        matrix_map = list_nums(name_of_txt_file_with_heights)
+        draw_map(matrix_map, name_of_map_image)
+        danger_matrix = list_nums(name_of_txt_file_with_danger_of_heights)
+        draw_danger(danger_matrix, name_of_depth_danger_image)
+        combine(name_of_map_image, name_of_depth_danger_image)
+
+
+    #if __name__ == '__main__':
+    full_map_conjunction()
+    return 0
+
--- TransformMatrix.py
+++ TransformMatrix.py
@@ -1,107 +1,162 @@
-import numpy
+def Ivan_func(filename, maxdep,mindep):
 
-path = 'map.txt'
-new_path = 'map1.txt'
-path_1_1 = 'map_1_1.txt'
-open_map = open(path, 'r')
-open_new_map = open(new_path, 'w+')
-open_path_1_1 = open(path_1_1,'w')
-map = open_map.readlines()
-open_new_map.truncate()
-open_path_1_1.truncate()
-new_map = open_new_map.readlines()
+    import numpy
 
-num_str = 0
-num_clm = 0
+    path = filename
+    new_path = 'DepthDangerZone.txt'
+    path_1_1 = 'map_1_1.txt'
+    open_map = open(path, 'r')
+    open_new_map = open(new_path, 'w+')
+    open_path_1_1 = open(path_1_1,'w')
+    map = open_map.readlines()
+    open_new_map.truncate()
+    open_path_1_1.truncate()
+    new_map = open_new_map.readlines()
 
-Max_Depth = 500
-Min_Depth = 100
+    num_str = 0
+    num_clm = 0
 
-
-
-def Warnings(Current_Depth):
-    W = (Max_Depth - Current_Depth) / (Max_Depth - Min_Depth)
-    if W > 1:
-        W = 1
-    if W < 0:
-        W = 0
-    return W
+    Max_Depth = int(maxdep)
+    Min_Depth = int(mindep)
 
 
-def Klass(tbd):
-    if tbd == 0:
-        return 0
-    if tbd > 0 and tbd <= 0.25:
-        return 0.25
-    if tbd > 0.25 and tbd <= 0.50:
-        return 0.5
-    if tbd > 0.50 and tbd <= 0.75:
-        return 0.75
-    if tbd > 0.75 and tbd <= 1.00:
-        return 1
 
-num_str = len(map)
-for i in map:
-    kol = 0
-    for j in i.split():
-        if j.isnumeric():
-            Current_Depth = int(j)
-            tbd = Warnings(Current_Depth)
-            open_new_map.write(str(Klass(tbd)) + '\t')
-            kol+=1
-    open_new_map.write('\n')
-    if (kol>num_clm):
-        num_clm = kol
-open_map.close()
-open_new_map.close()
+    def Warnings(Current_Depth):
+        W = (Max_Depth - Current_Depth) / (Max_Depth - Min_Depth)
+        if W > 1:
+            W = 1
+        if W < 0:
+            W = 0
+        return W
 
-open_new_map = open(new_path,"r")
-new_map = open_new_map.readlines()
-mas = numpy.zeros((num_str+1,num_clm))
-x = 0
-y = 0
-for i in new_map:
+
+    def Klass(tbd):
+        if tbd == 0:
+            return 0
+        if tbd > 0 and tbd <= 0.25:
+            return 0.25
+        if tbd > 0.25 and tbd <= 0.50:
+            return 0.5
+        if tbd > 0.50 and tbd <= 0.75:
+            return 0.75
+        if tbd > 0.75 and tbd <= 1.00:
+            return 1
+
+    num_str = len(map)
+    for i in map:
+        kol = 0
+        for j in i.split():
+            if j.isnumeric():
+                Current_Depth = int(j)
+                tbd = Warnings(Current_Depth)
+                open_new_map.write(str(Klass(tbd)) + '\t')
+                kol+=1
+        open_new_map.write('\n')
+        if (kol>num_clm):
+            num_clm = kol
+    open_map.close()
+    open_new_map.close()
+
+    open_new_map = open(new_path,"r")
+    new_map = open_new_map.readlines()
+    mas = numpy.zeros((num_str,num_clm))
+    x = 0
     y = 0
-    for j in i.split():
-         if j != ' ':
-             mas[x][y] = float(j)
-             # print(j)
-             y+=1
-    x+=1
-open_new_map.close()
-bul_mas = numpy.zeros((num_str,num_clm))
-# for i in range(num_str):
-#     for j in range(num_clm):
-def Rekyrs(index,jndex):
-    bul_mas[index][jndex]+=1
-    if bul_mas[index][jndex]>=3:
-         return 0
-    if index == num_str-1 and jndex == 0:
-        if mas[num_str-1][0] < mas[num_str-1][1] and mas[num_str-1][0]<mas[num_str-2][0]:
-            mas[num_str-1][0]+=0.25
-    if index == num_str-1 and jndex !=0:
-        if mas[num_str-1][jndex]<mas[num_str][jndex-1] and mas[num_str-1][jndex]<mas[num_str][jndex+1]:
-            mas[num_str-1][jndex]+=0.25
-    if index == 0 and jndex == num_clm-1:
-        if mas[0][num_clm-1] < mas[0][jndex - 2] and mas[0][num_clm-1] < mas[1][num_clm-1]:
-            mas[0][num_clm-1]+=0.25
-    if index != 0 and jndex == num_clm-1:
-        if mas[index][num_clm-1]<mas[index-1][num_clm-1] and mas[index][num_clm-1]<mas[index+1][num_clm-1]:
-            mas[index][num_clm-1]+=0.25
-    else:
-        if mas[index][jndex]<mas[index+1][jndex] and mas[index][jndex] < mas[index][jndex+1]:
-            mas[index][jndex]+=0.25
-    if index<num_str-1:
-        Rekyrs(index+1,jndex)
-    if jndex<num_clm-1:
-        Rekyrs(index,jndex+1)
+    for i in new_map:
+        y = 0
+        for j in i.split():
+             if j != ' ':
+                 mas[x][y] = float(j)
+                 # print(j)
+                 y+=1
+        x+=1
+    open_new_map.close()
+    bul_mas = numpy.zeros((num_str,num_clm))
+    new_mas = mas
+    def determinant_i (a):
+        if a < 0 or a > num_str-1:
+            return 0
+        else:
+            return 1
+
+    def determinant_j (a):
+        if a < 0 or a > num_clm-1:
+            return 0
+        else:
+            return 1
+
+    def check(index,jndex,ind,jnd):
+        if mas[index][jndex]<mas[ind][jnd]:
+            bul_mas[index][jndex]+=1
+
+    def check_neighbors(index,jndex):
+        k = 0
+        if determinant_i(index-1):
+            check(index,jndex,index-1,jndex)
+            k+=1
+        # if determinant(index-1) and determinant(jndex+1):
+        #     check(index,jndex,index-1,jndex+1)
+        #     k+=1
+        if determinant_j(jndex+1):
+            check(index,jndex,index,jndex+1)
+            k+=1
+        # if determinant(index+1) and determinant(jndex+1):
+        #     check(index,jndex,index+1,jndex+1)
+        #     k+=1
+        if determinant_i(index+1):
+            check(index,jndex,index+1,jndex)
+            k+=1
+        # if determinant(index+1) and determinant(jndex-1):
+        #     check(index,jndex,index+1,jndex-1)
+        #     k+=1
+        if determinant_j(jndex-1):
+            check(index,jndex,index,jndex-1)
+            k+=1
+        # if determinant(index-1) and determinant(jndex-1):
+        #     check(index,jndex,index-1,jndex-1)
+        #     k+=1
+        return k
+
+    def last_check(i,j):
+        tbd = check_neighbors(i,j)
+        if tbd==2:
+            if bul_mas[i][j] == 2:
+                mas[i][j]+=0.25
+        if tbd==3:
+            if bul_mas[i][j] == 3:
+                mas[i][j]+=0.25
+        if tbd==4:
+            if bul_mas[i][j]>=3:
+                mas[i][j]+=0.25
+        return tbd
+
+    def launch(i,j):
+        flag = 1
+        while (flag):
+            flag = 0
+            bul_mas[i][j] = 0
+            tbd = mas[i][j]
+            last_check(i, j)
+            if tbd != mas[i][j]:
+                flag = 1
+                if determinant_i(i - 1):
+                    launch(i - 1, j)
+                if determinant_j(j + 1):
+                    launch(i, j + 1)
+                if determinant_i(i + 1):
+                    launch(i + 1, j)
+                if determinant_j(j - 1):
+                    launch(i, j - 1)
+
+    for i in range(num_str-1):
+        for j in range(num_clm-1):
+            launch(i,j)
+
+
+    for i in range(num_str):
+        for j in range(num_clm):
+            open_path_1_1.write(str(mas[i][j]) + '\t')
+        open_path_1_1.write('\n')
+    open_path_1_1.close()
+
     return 0
-
-
-Rekyrs(0,0)
-
-for i in range(num_str):
-    for j in range(num_clm):
-        open_path_1_1.write(str(mas[i][j]) + '\t')
-    open_path_1_1.write('\n')
-open_path_1_1.close()
