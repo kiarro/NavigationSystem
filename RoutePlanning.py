3f0806ba56197db7ebd8689c5fa7290805a34124
Юрий <38157715+kiarro@users.noreply.github.com>
2019-04-19 21:47:14 +0300

    Add files via upload
    New, more effective, algorithm

--- RoutePlanning.py
+++ RoutePlanning.py
@@ -7,9 +7,18 @@ import scipy.optimize as opt
 from enum import Enum
 import Graph
 
+eps = 0.0001
+
 class Direction(Enum):
     CONTERCW = 1
     CLOCKWISE = 2
+
+class MainPointInBadZoneError(Exception):
+    def __init__(self, text):
+        self.txt = text
+
+class RoutePlanSoftError(Exception):
+    pass
 
 class Arc():
     def __init__(self, X0, Y0, R, A1, A2, DIR):
@@ -50,7 +59,7 @@ def isCrossed(line, round):
     # Looks where route_line intersect round and if this point is in interval
     solution = opt.fsolve(lambda var: [(var[0] - round[0]) ** 2 + ((var[1] - round[1]) ** 2) - round[2] ** 2,
                                        (var[0] - line[0]) * (line[3] - line[1]) - (var[1] - line[1]) * (
-                                                   line[2] - line[0])], [0, 0])
+                                                   line[2] - line[0])], [round[0], round[0]])
 
     if solution[0] >= line[0] and solution[0] >= line[2] or \
             solution[0] <= line[0] and solution[0] <= line[2] or \
@@ -69,27 +78,41 @@ def clarifyRoute(route, round_array):
     previous_arc = route[0]
     arc_number = 0
     for arc in route:
-        if arc_number>0 and arc_number<len(route)-1:
-            a_1, a_2, d_1, d_2 = createCompound(route[arc_number-1], route[arc_number+1])
-            # print("---------------------------------------------------------------------------------")
-            # print("round 1 = {}, round 2 = {}, angle 1 = {}, angle 2 = {}".format([route[arc_number-1].x0, route[arc_number-1].y0, route[arc_number-1].r], [route[arc_number+1].x0, route[arc_number+1].y0, route[arc_number+1].r], a_1*180/numpy.pi, a_2*180/numpy.pi))
-            line = (route[arc_number-1].x0 + route[arc_number-1].r * numpy.cos(a_1),
-                    route[arc_number-1].y0 + route[arc_number-1].r * numpy.sin(a_1), route[arc_number+1].x0 + route[arc_number+1].r * numpy.cos(a_2),
-                    route[arc_number+1].y0 + route[arc_number+1].r * numpy.sin(a_2))
-            if not isCrossed(line, [route[arc_number].x0, route[arc_number].y0, route[arc_number].r]):
-                route[arc_number - 1].a2 = a_1
-                route[arc_number + 1].a1 = a_2
-                route.pop(arc_number)
-                # print("--")
-                num_of_clarify += 1
-                # return num_of_clarify
+        # delete extra point of route
+        try:
+            if arc_number>0 and arc_number<len(route)-1:
+                if previous_arc.x0 == arc.x0 and previous_arc.y0 == arc.y0 and previous_arc.r == arc.r:
+                    previous_arc.a2 = arc.a2
+                    raise RoutePlanSoftError
+
+                a_1, a_2, d_1, d_2 = createCompound(route[arc_number-1], route[arc_number+1])
+                # print("---------------------------------------------------------------------------------")
+                # print("round 1 = {}, round 2 = {}, angle 1 = {}, angle 2 = {}".format([route[arc_number-1].x0, route[arc_number-1].y0, route[arc_number-1].r], [route[arc_number+1].x0, route[arc_number+1].y0, route[arc_number+1].r], a_1*180/numpy.pi, a_2*180/numpy.pi))
+                line = (route[arc_number-1].x0 + route[arc_number-1].r * numpy.cos(a_1),
+                        route[arc_number-1].y0 + route[arc_number-1].r * numpy.sin(a_1), route[arc_number+1].x0 + route[arc_number+1].r * numpy.cos(a_2),
+                        route[arc_number+1].y0 + route[arc_number+1].r * numpy.sin(a_2))
+                if not isCrossed(line, [route[arc_number].x0, route[arc_number].y0, route[arc_number].r]):
+                    route[arc_number - 1].a2 = a_1
+                    route[arc_number + 1].a1 = a_2
+                    # print("--")
+                    # return num_of_clarify
+                    raise RoutePlanSoftError
+        except RoutePlanSoftError:
+            route.pop(arc_number)
+            num_of_clarify += 1
+
+        if not (previous_arc.a2 == arc.a1 or previous_arc.a2 == (arc.a1 + numpy.pi)%(numpy.pi*2)):
+            print("{}, {}, {}, {} - {}, {}, {}, {}".format(previous_arc.x0, previous_arc.y0, previous_arc.r, previous_arc.a2, arc.x0, arc.y0, arc.r, arc.a1))
+            a_1, a_2, d1, d2 = createCompound(previous_arc, arc)
+            previous_arc.a2 = a_1
+            arc.a1 = a_2
 
         line = (previous_arc.x0+previous_arc.r*numpy.cos(previous_arc.a2), previous_arc.y0+previous_arc.r*numpy.sin(previous_arc.a2), arc.x0+arc.r*numpy.cos(arc.a1), arc.y0+arc.r*numpy.sin(arc.a1))
         for round in round_array:
             # Looks if this round is edge of line
-            if previous_arc.x0 == round[0] and previous_arc.y0 == round[1] and previous_arc.r == round[2] or arc.x0 == \
-                    round[0] and arc.y0 == round[1] and arc.r == round[2]:
-                # print("{} have edge at {}. So their cross is {}.".format(line, round, cross))
+            if previous_arc.x0 == round[0] and previous_arc.y0 == round[1] and previous_arc.r == round[2]:
+                continue
+            if arc.x0 == round[0] and arc.y0 == round[1] and arc.r == round[2]:
                 continue
             if not isCrossed(line, round):
                 continue
@@ -124,13 +147,15 @@ def clarifyRoute(route, round_array):
         previous_arc = arc
         arc_number = arc_number + 1
         # previous_point = point
+    route[0].a1 = route[0].a2
+    route[len(route)-1].a2 = route[len(route)-1].a1
     return num_of_clarify
 
 # construct compounds (angel) between two circles and find more fit
 def createCompound(arc1, arc2):
 
-    if arc1.x0 == 500 and arc1.y0 == 400 and arc2.x0 == 500 and arc2.y0 == 460:
-        print("-")
+    # if arc1.x0 == 500 and arc1.y0 == 400 and arc2.x0 == 500 and arc2.y0 == 460:
+    #     print("-")
 
     # find parameters of equivalent construction task with a single round and a point
     if arc1.r>arc2.r:
@@ -216,25 +241,41 @@ def createTangent(XA, YA, X0, Y0, R):
 
 # main function that create route throuth field
 def CreateRoute(startX, startY, endX, endY, width, height, red_zone):
-    pointed_route = list()
-    pointed_route.append(Arc(startX, startY, 0, 0, 0, Direction.CONTERCW))
-    pointed_route.append(Arc(endX, endY, 0, 0, 0, Direction.CONTERCW))
-    red_zone = sorted(red_zone, key=lambda round: round[2], reverse=True)
-    n=1
-    while n>0:
-        n = clarifyRoute(pointed_route, red_zone)
+    try:
+        pointed_route = list()
+        pointed_route.append(Arc(startX, startY, 0, 0, 0, Direction.CONTERCW))
+        pointed_route.append(Arc(endX, endY, 0, 0, 0, Direction.CONTERCW))
+        red_zone = sorted(red_zone, key=lambda round: round[2], reverse=True)
+        if startX < 0 or startX > width or startY < 0 or startY > height:
+            raise MainPointInBadZoneError("Start point lay outside map")
+        if endX < 0 or endX > width or endY < 0 or endY > height:
+            raise MainPointInBadZoneError("End point lay outside map")
+        for i_circle in red_zone:
+            r = numpy.sqrt((startX - i_circle[0])**2 + (startY - i_circle[1])**2)
+            if r < i_circle[2]:
+                raise MainPointInBadZoneError("Start point lay into interdicted zone")
+            r = numpy.sqrt((endX - i_circle[0]) ** 2 + (endY - i_circle[1]) ** 2)
+            if r < i_circle[2]:
+                raise MainPointInBadZoneError("End point lay into interdicted zone")
+        n=1
+        while n>0:
+            n = clarifyRoute(pointed_route, red_zone)
+    except MainPointInBadZoneError as err:
+        print("Error: {}".format(err))
+        pointed_route[1]=pointed_route[0]
     return pointed_route
 
 
 if __name__ == "__main__":
     # route = [Arc(0,0,0,0,0,Direction.CONTERCW),Arc(599,599,0,0,0,Direction.CONTERCW)]
-    rounds = [(85,80,6), (15, 16, 9), (50, 0, 8), (60, 62, 4), (200, 100, 85), (400, 360, 76), (500, 100, 50), (560, 540, 15), (380, 350, 6), (500, 460, 80), (200, 280, 30), (360, 450, 100), (500, 400, 60)]
+    rounds = [(85,80,6), (15, 16, 9), (50, 0, 8), (60, 62, 4), (200, 100, 85), (400, 360, 76), (500, 100, 50), (560, 540, 15), (380, 350, 6), (200, 280, 30), (360, 450, 100), (500, 400, 60)]
     # rounds = [(90, 60, 20), (150, 150, 30), (500, 540, 50), (300, 340, 60)]
+    # rounds = [(200, 0, 20), (200, 20, 20), (200, 50, 20), (200, 80, 20), (200, 100, 20), (200, 110, 20), (200, 145, 20), (200, 160, 20), (210, 180, 20), (200, 200, 20), (200, 235, 20), (195, 265, 20), (200, 300, 20)]
     # rounds = sorted(rounds, key=lambda round: round[2], reverse=True)
     print(rounds)
 
 
-    x_start, y_start, x_end, y_end = 0, 0, 599, 599
+    x_start, y_start, x_end, y_end = 0, 0, 500, 500
     width, height = 600, 600
     route = CreateRoute(x_start,y_start,x_end,y_end,width,height,rounds)
     #
@@ -256,23 +297,3 @@ if __name__ == "__main__":
     for i in route:
         Graph.draw_line((int)(i_pr.x0 + (i_pr.r + 0.51) * numpy.cos(i_pr.a2)), (int)(i_pr.y0 + (i_pr.r + 0.51) * numpy.sin(i_pr.a2)), (int)(i.x0 + (i.r + 0.51) * numpy.cos(i.a1)), (int)(i.y0 + (i.r + 0.51) * numpy.sin(i.a1)))
         i_pr = i
-
-
-    # print(createTangent(0,0,10,10,10))
-    # arc1 = Arc(0,0,5,0,numpy.pi/3*2,Direction.CLOCKWISE)
-    # arc2 = Arc(10,15,15,numpy.pi/2,0,Direction.CLOCKWISE)
-    # print(createCompound(arc1, arc2))
-    # print(numpy.nan > 0)
-    # print(numpy.nan > 100)
-    # print(numpy.nan < 0)
-    # print(numpy.nan < 100)
-    # print(numpy.nan > numpy.nan)
-    # print(numpy.nan < numpy.nan)
-    # print(numpy.nan == numpy.nan)
-    # print(0 > numpy.nan)
-    # print(100 > numpy.nan)
-    # print(0 < numpy.nan)
-    # print(100 < numpy.nan)
-    # print(numpy.nan + 100)
-    # print(numpy.nan + 100 > 120)
-    # print(numpy.nan + 100 > 0)
